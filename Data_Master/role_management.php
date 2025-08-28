334a47bc40ec1449c1c339784f3a39caa8fe9311
ZaganJade <playerthebest042@gmail.com>
2025-08-28 13:58:46 +0700

    update role href

--- Data_Master/role_management.php
+++ Data_Master/role_management.php
@@ -84,7 +84,8 @@ if (isset($_GET['delete'])) {
                                 <?= $r['status'] ? 'Nonaktifkan' : 'Aktifkan' ?>
                             </a>
                             <a href="?delete=<?= $r['idrole_user'] ?>" class="btn delete"
-                                onclick="return confirm('Hapus role ini dari user?')">Hapus</a>     
+                                onclick="return confirm('Hapus role ini dari user?')">Hapus</a> 
+                                <a href="knfjlsekf"></a>    
                         </div>
                     </td>
                 </tr>
